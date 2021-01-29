e3d5d3e60860b98d81726fbab91d71730a9396fa
ARIF - 404 <78012268+termxteam@users.noreply.github.com>
2021-01-29 12:39:01 +0700

    Add files via upload

---
+++ HACKV2.sh
@@ -0,0 +1,221 @@
+#bin/bash/Hacker_Boss
+clear
+echo
+figlet -f slant " ARIF 02"
+sleep 2
+echo "[●]■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■[●]" | lolcat
+echo "[●]          GUNAKAN DENGAN BAIK               [●]" | lolcat
+echo "[●]■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■[●]" | lolcat
+echo "[●] • Author   : ARIF 02                       [●]" | lolcat
+echo "[●] • Youtube  : HackerBoss                    [●]" | lolcat
+echo "[●] • Group    : TERMUX°CYBER°TEAM             [●]" | lolcat
+echo "[●]■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■[●]" | lolcat
+echo "[●]     •JANGAN LUPA SUBSCRIBE Hacker Boss•    [●]" | lolcat
+echo "[●]■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■[●]" | lolcat
+echo "
+┏━━━━━┫{menu dark fb}┣━━━━━┓
+┃
+┠──[00] untuk instal bahan agar work ✔
+┃
+┠──[01] Dark 1.6 ✔
+┃
+┠──[02] Dark 1.7 [-FREE-] ✔
+┃
+┠──[03] Dark 1.8 ✔
+┃
+┠──[04] Dark premium ✔
+┃
+┠──[05] Dark VIP ✔
+┃
+┠──[06] Dark pro ✔
+┃
+┠──[07] Dark Diamond ✔
+┃
+┠──[08] Dark v.1.2 ✔
+┠
+┠──[09] v2.Dark ✔
+┠
+┠──[10] Dark-Cyber11 ✔
+┠
+┠──[11] cyber 2.5 ✔
+┠
+┠──[12] IBLI2
+┠
+┠──[13] Dark-Asu
+┠
+┠──[14] BHV26
+┠
+┠──[15] MR.Rendy-Dark
+┠
+┗────[99] exit ✘" | lolcat -a -d 10
+echo
+read -p "root@ARIF 02>> " bro
+
+if [ $bro = 0 ] || [ $bro]
+then
+clear
+pkg update && pkg upgrade
+pkg install python2
+pkg install bash
+pkg install git
+pkg install python
+pkg install wget
+pkg install curl
+pkg install nano
+pkg install figlet
+pkg install toilet
+pkg install unzip
+pkg install cowsay
+pkg install zip
+pkg install openssh
+pkg install php
+pkg install ruby
+gem install lolcat
+pip2 install requests
+pip2 install mechanize
+bash HACKV2.sh
+fi
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+pkg install git
+git clone  https://github.com/pashayogi/SETAN
+cd SETAN
+python2 SETAN.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+pkg install git
+git clone https://github.com/muhalvin/darkfb7
+cd darkfb7
+python2 dark.py
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+pkg install git
+git clone https://github.com/Mr-XsZ/Dark-Fb
+cd Dark-Fb
+python2 dark.py
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+pkg install git
+git clone https://github.com/TheMagizz/DarkPremium
+cd DarkPremium
+python2 DarkFB.py
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+pkg install git
+git clone https://github.com/m4rche3ll-cyber/dark-vpro
+cd dark-vpro
+python2 dark-vpro.py
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+git clone https://github.com/wira2611/Pro2611
+cd Pro2611
+python2 Pro.py
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+git clone https://github.com/wira2611/diamon
+ls
+cd diamon
+ls
+python2 diamond-1.py
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+git clone https://github.com/cyber2611/v1.2
+ls
+cd v1.2
+ls
+python2 Dragon-v1.2.py
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+git clone https://github.com/cyber2611/v2-dark
+cd v2.dark
+ls
+python2 v2.dark.py
+if
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+git clone https://github.com/cyber2611/dark-cyber11
+cd dark-cyber11
+ls
+python2 kapten.py
+if
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+git clone https://github.com/cyber2611/cyber2.5
+cd cyber2.5
+ls
+python2 Premium2.5.py
+if
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+git clone https://github.com/cyber2611/IBLI2
+cd IBLI2
+ls
+python2 dark-iblis.py
+if
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+git clone https://github.com/cyber2611/dark-asu
+cd dark-asu
+ls
+python2 kimak.py
+if
+
+if if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+git clone https://github.com/cyber2611/BHV26
+cd BHV26
+ls
+python2 vip-1.9.py
+if
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+git clone https://github.com/cyber2611/Mr.RENDY
+cd Mr.RENDY
+ls
+python2 Mr.rendy.py
+if
+
+if [ $bro = 99 ] || [ $bro = 99 ]
+then
+echo $cyan  "THANKS BOSKU DAN JANGAN LUPA SUBSCRIBE"
+sleep 1
+exit
+fi
+
